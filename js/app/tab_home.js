15cef8f8eae2aafa69ae36e3184710bcf8c8505a
songyanming <1062608083@qq.com>
2016-09-05 16:39:32 +0800

    提醒界面

--- js/app/tab_home.js
+++ js/app/tab_home.js
@@ -3,6 +3,47 @@ mui.init({
 });
 
 mui.plusReady(function() {
+mui(".mui-table-view").on('tap','.mui-table-view-cell',function(){
+  //获取id
+  var id = this.getAttribute("id");
+  switch (id)
+{
+case "my_job0":
+	mui.openWindow({
+    id:id,
+    url:'my_job.html'
+  });
+  break;
+case "my_job1":
+	mui.openWindow({
+    id:id,
+    url:'campus_notice.html'
+  });
+  break;
+case "my_job2":
+	mui.openWindow({
+    id:id,
+    url:'class_post.html'
+  });
+  break;
+case "my_job3":
+	mui.openWindow({
+    id:id,
+    url:'school_blog.html'
+  });
+  break;
+case "my_job4":
+	mui.openWindow({
+    id:id,
+    url:'sunshine_sports.html'
+  });
+  break;
+
+}
+  
+
+
+})
 	//点击左上角图标，打开帮助；
 	document.getElementById('help').addEventListener('tap', function() {
 		console.log("help");
