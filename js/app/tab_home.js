7e5d99286a772e85a21eb871b6ed113727e90cbd
songyanming <1062608083@qq.com>
2016-09-06 11:35:38 +0800

    我的作业界面

--- js/app/tab_home.js
+++ js/app/tab_home.js
@@ -9,15 +9,21 @@ mui(".mui-table-view").on('tap','.mui-table-view-cell',function(){
   switch (id)
 {
 case "my_job0":
-	mui.openWindow({
-    url:'my_job.html'
-  });
+
+//把子页的路径写在数组里面
+var subpages = ['tab_home.html', 'tab_class.html', 'tab_application.html', 'tab_me.html'];
+	//显示目标选项卡
+	plus.webview.show(subpages[1]);
+	//隐藏当前选项卡
+	plus.webview.hide(subpages[0]);
+
   break;
 case "my_job1":
+
 	mui.openWindow({
     id:id,
     url:'campus_notice.html'
-  });
+});
   break;
 case "my_job2":
 	mui.openWindow({
@@ -43,6 +49,7 @@ case "my_job4":
 
 
 })
+
 	//点击左上角图标，打开帮助；
 	document.getElementById('help').addEventListener('tap', function() {
 		console.log("help");
