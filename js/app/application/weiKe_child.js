4abd3f7d544bf0cb427b1ed1dd342172318b708d
rockan007 <rockan007@gmail.com>
2016-09-08 15:07:51 +0800

    发的

--- js/app/application/weiKe_child.js
+++ js/app/application/weiKe_child.js
@@ -1,43 +1,17 @@
 
 mui.plusReady(
-		var supPage=plus.webview.currentWebview().opener();
-		var all_grade=document.getElementById("all_grade");
-		var one=document.getElementById("one");
-		var two=document.getElementById("two");
-		var three=document.getElementById("three");
-		var four=document.getElementById("four");
-		var five=document.getElementById("five");
-		var six=document.getElementById("six");
+	
 	function(){
+		var supPage=plus.webview.getWebviewById("html/application/weiKe.html");
+		var list = document.querySelector('.mui-table-view.mui-table-view-radio');
 		
-		all_grade.addEventListener('tap',function(){
-			freshWeiKe("全部年级")
+		list.addEventListener('selected',function(e){
+			mui.fire(supPage,"pageRefresh",{
+				id:e.detail.el.innerText
+			} )
+			mui.redirect('weiKe','html/application/weiKe.html');
 		})
-		one.addEventListener('tap',function(){
-			freshWeiKe("一年级")
-		})
-		two.addEventListener("tap",function(){
-			freshWeiKe("二年级")
-		})
-		three.addEventListener('tap',function(){
-			freshWeiKe("三年级")
-		})
-		four.addEventListener('tap',function(){
-			freshWeiKe("四年级")
-		})
-		five.addEventListener('tap',function(){
-			freshWeiKe("五年级")
-		})
-		six.addEventListener('tap',function(){
-			freshWeiKe("六年级")
-		})
-		function freshWeiKe(fag){
-		mui.fire(supPage,"pageRefresh",{
-			id:fag
-		});
-		mui.redirect('weiKe','weiKe.html')
 
-	}
 	}
 	
 )
