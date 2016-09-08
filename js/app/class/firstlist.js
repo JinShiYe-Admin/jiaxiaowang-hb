096c17996c045074dab05681aff3db321aec5a1e
rockan007 <rockan007@gmail.com>
2016-09-08 09:00:45 +0800

    不知道改了啥

--- js/app/class/firstlist.js
+++ js/app/class/firstlist.js
@@ -3,7 +3,7 @@ mui.init({
 		container: "#firstlist", //下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
 		up: {
 			contentrefresh: "正在加载...", //可选，正在加载状态时，上拉加载控件上显示的标题内容
-			contentnomore: '没有更多数据了', //可选，请求完毕若没有更多数据时显示的提醒内容；
+			contentnomore: "没有更多数据了", //可选，请求完毕若没有更多数据时显示的提醒内容；
 			callback: pullupRefresh
 		},
 		down: {
