f2c1a1d240d4429ad0771c00d4c74654c23b702a
rockan007 <rockan007@gmail.com>
2016-09-09 10:48:21 +0800

    登录角色选择

---
+++ js/login/reg.js
@@ -0,0 +1,25 @@
+mui.plusReady(
+	//加载下一步的监听事件
+	function(){
+		var btn_nextReg=document.getElementById('reg');
+		reg.addEventListener('tap',function(){
+			
+		})
+	}
+)
+//获取各输入框内容
+function getInput(){
+	
+}
+//判断内容是否符合要求
+function judgeInput(){
+	
+}
+//判断是不是邮箱
+function isEmail(email){
+	
+}
+//判断注册码正确与否
+function isRegisterNo(){
+	
+}
