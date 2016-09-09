0151af2ebf101bfe037f646e2008a4cfd8947d13
Zqw <578159259@qq.com>
2016-09-09 17:09:26 +0800

    更新

--- js/app/class/myclasssecond.js
+++ js/app/class/myclasssecond.js
@@ -39,7 +39,7 @@ mui.init({
 		longtap: true //开启长按监听
 	},
 	pullRefresh: {
-		container: "#content", //通过id找到下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
+		container: "#slider", //通过id找到下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
 		//		up: {
 		//			contentrefresh: "正在加载...", //可选，正在加载状态时，上拉加载控件上显示的标题内容
 		//			contentnomore: '没有更多数据了', //可选，请求完毕若没有更多数据时显示的提醒内容；
