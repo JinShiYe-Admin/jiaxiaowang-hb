7350ed6f91c173fbd9ea71a969222f0a11bfa77e
songyanming <1062608083@qq.com>
2016-09-12 17:13:10 +0800

    上传照片文件 并添加到本地数据库数据库（增删改查）

--- js/app/remind/newPost.js
+++ js/app/remind/newPost.js
@@ -1,10 +1,13 @@
-mui.init({
+
+(function() {
+	mui.init({
 	swipBack: false
 })
 mui.plusReady(
 	function() {
-		var subcategory = document.getElementById("subcategory");
 
+		var subcategory = document.getElementById("subcategory");
+      //点击未分类
 		document.getElementById("category").addEventListener('tap', function() {
 			console.log(789);
 			mui.openWindow({
@@ -13,6 +16,7 @@ mui.plusReady(
 			});
 
 		})
+		//选择分类后刷新数据
 		window.addEventListener('pageflowrefresh', function(e) {
 			console.log(9);
 			if(typeof(e.detail.data) !== 'undefined') {
@@ -25,3 +29,254 @@ mui.plusReady(
 		})
 	}
 )
+	var index = 1;
+	var size = null;
+	var imageIndexIdNum = 0;
+	var starIndex = 0;
+	var feedback = {
+		subcategory:document.getElementById('subcategory'),
+		title: document.getElementById('title'), 
+		content: document.getElementById('content'), 
+		imageList: document.getElementById('image-list'),
+		submitBtn: document.getElementById('submit')
+	};
+	var url = 'https://service.dcloud.net.cn/feedback';
+	feedback.files = [];
+	feedback.uploader = null;  
+	feedback.deviceInfo = null; 
+	mui.plusReady(function() {
+		//设备信息，无需修改
+		feedback.deviceInfo = {
+			appid: plus.runtime.appid, 
+			imei: plus.device.imei, //设备标识
+			images: feedback.files, //图片文件
+			p: mui.os.android ? 'a' : 'i', //平台类型，i表示iOS平台，a表示Android平台。
+			md: plus.device.model, //设备型号
+			app_version: plus.runtime.version,
+			plus_version: plus.runtime.innerVersion, //基座版本号
+			os:  mui.os.version,
+			net: ''+plus.networkinfo.getCurrentType()
+		}
+	});
+	/**
+	 *提交成功之后，恢复表单项 
+	 */
+	feedback.clearForm = function() {
+		feedback.subcategory.value = '';
+		feedback.title.value = '';
+		feedback.content.value = '';
+		feedback.imageList.innerHTML = '';
+		feedback.newPlaceholder();
+		feedback.files = [];
+		index = 0;
+		size = 0;
+		imageIndexIdNum = 0;
+		starIndex = 0;
+
+	};
+	feedback.getFileInputArray = function() {
+		return [].slice.call(feedback.imageList.querySelectorAll('.file'));
+	};
+	feedback.addFile = function(path) {
+		feedback.files.push({name:"images"+index,path:path});
+		index++;
+	};
+	/**
+	 * 初始化图片域占位
+	 */
+	feedback.newPlaceholder = function() {
+		var fileInputArray = feedback.getFileInputArray();
+		if (fileInputArray &&
+			fileInputArray.length > 0 &&
+			fileInputArray[fileInputArray.length - 1].parentNode.classList.contains('space')) {
+			return;
+		};
+		imageIndexIdNum++;
+		var placeholder = document.createElement('div');
+		placeholder.setAttribute('class', 'image-item space');
+		var up = document.createElement("div");
+		up.setAttribute('class','image-up')
+		//删除图片
+		var closeButton = document.createElement('div');
+		closeButton.setAttribute('class', 'image-close');
+		closeButton.innerHTML = 'X';
+		//小X的点击事件
+		closeButton.addEventListener('tap', function(event) {
+			setTimeout(function() {
+				feedback.imageList.removeChild(placeholder);
+			}, 0);
+			return false;
+		}, false);
+		
+		//
+		var fileInput = document.createElement('div');
+		fileInput.setAttribute('class', 'file');
+		fileInput.setAttribute('id', 'image-' + imageIndexIdNum);
+		fileInput.addEventListener('tap', function(event) {
+			 			var self = this;
+			var index = (this.id).substr(-1);
+            if (mui.os.plus) { 
+                var a = [{ 
+                    title: "拍照" 
+                }, { 
+                    title: "从手机相册选择" 
+                }]; 
+                plus.nativeUI.actionSheet({ 
+                    title: "请选择图片位置", 
+                    cancel: "取消", 
+                    buttons: a 
+                }, function(b) { /*actionSheet 按钮点击事件*/ 
+                    switch (b.index) { 
+                        case 0: 
+                            break; 
+                        case 1: 
+			plus.camera.getCamera().captureImage(function(e) {
+				console.log("event:"+e);
+				var name = e.substr(e.lastIndexOf('/') + 1);
+				console.log("name:"+name);
+					
+				plus.zip.compressImage({
+					src: e,
+					dst: '_doc/' + name,
+					overwrite: true,
+					quality: 50
+				}, function(zip) {
+					size += zip.size  
+					console.log("filesize:"+zip.size+",totalsize:"+size);
+					if (size > (10*1024*1024)) {
+						return mui.toast('文件超大,请重新选择~');
+					}
+					if (!self.parentNode.classList.contains('space')) { //已有图片
+						feedback.files.splice(index-1,1,{name:"images"+index,path:e});
+					} else { //加号
+						placeholder.classList.remove('space');
+						feedback.addFile(zip.target);
+						feedback.newPlaceholder();
+					}
+					up.classList.remove('image-up');
+					placeholder.style.backgroundImage = 'url(' + zip.target + ')';
+				}, function(zipe) {
+					mui.toast('压缩失败！')
+				});
+				
+
+				
+			}, function(e) {
+				mui.toast(e.message);
+			},{});
+			break;
+                        case 2: 
+			plus.gallery.pick(function(e) {
+				console.log("event:"+e);
+				var name = e.substr(e.lastIndexOf('/') + 1);
+				console.log("name:"+name);
+					
+				plus.zip.compressImage({
+					src: e,
+					dst: '_doc/' + name,
+					overwrite: true,
+					quality: 50
+				}, function(zip) {
+					size += zip.size  
+					console.log("filesize:"+zip.size+",totalsize:"+size);
+					if (size > (10*1024*1024)) {
+						return mui.toast('文件超大,请重新选择~');
+					}
+					if (!self.parentNode.classList.contains('space')) { //已有图片
+						feedback.files.splice(index-1,1,{name:"images"+index,path:e});
+					} else { //加号
+						placeholder.classList.remove('space');
+						feedback.addFile(zip.target);
+						feedback.newPlaceholder();
+					}
+					up.classList.remove('image-up');
+					placeholder.style.backgroundImage = 'url(' + zip.target + ')';
+				}, function(zipe) {
+					mui.toast('压缩失败！')
+				});
+				
+
+				
+			}, function(e) {
+				mui.toast(e.message);
+			},{}); 
+			break;
+                        default: 
+                            break; 
+                    } 
+                }) 
+            } 
+
+			
+
+		}, false);
+		placeholder.appendChild(closeButton);
+		placeholder.appendChild(up);
+		placeholder.appendChild(fileInput);
+		feedback.imageList.appendChild(placeholder);
+	};
+	feedback.newPlaceholder();
+	feedback.submitBtn.addEventListener('tap', function(event) {
+
+					if (feedback.title.value == ''  ) {
+			return mui.toast('标题不能为空');
+		}
+					if (feedback.content.value == '' ) {
+			return mui.toast('正文不能为空');
+		}
+
+					if (feedback.subcategory.innerHTML == '未分类'  ) {
+			return mui.toast('请选择分类');
+		}
+
+		//判断网络连接
+		if(plus.networkinfo.getCurrentType()==plus.networkinfo.CONNECTION_NONE){
+			return mui.toast("连接网络失败，请稍后再试");
+		}
+		feedback.send(mui.extend({}, feedback.deviceInfo, {
+			content: feedback.title.value,
+			content: feedback.content.value,
+			images: feedback.files,
+		})) 
+	}, false)
+	feedback.send = function(content) {
+		feedback.uploader = plus.uploader.createUpload(url, {
+			method: 'POST'
+		}, function(upload, status) {
+//			plus.nativeUI.closeWaiting()
+			console.log("upload cb:"+upload.responseText);
+			if(status==200){
+				var data = JSON.parse(upload.responseText);
+				//上传成功，重置表单
+				if (data.ret === 0 && data.desc === 'Success') {
+					mui.toast('上传成功~')
+					console.log("upload success");
+//					feedback.clearForm();
+				}
+			}else{
+				console.log("upload fail");
+			}
+			
+		});
+		//添加上传数据
+		mui.each(content, function(index, element) {
+			if (index !== 'images') {
+				console.log("addData:"+index+","+element);
+//				console.log(index);
+				feedback.uploader.addData(index, element)
+			} 
+		});
+		//添加上传文件
+		mui.each(feedback.files, function(index, element) {
+			var f = feedback.files[index];
+			console.log("addFile:"+JSON.stringify(f));
+			feedback.uploader.addFile(f.path, {
+				key: f.name
+			});
+		});
+		//开始上传任务
+		feedback.uploader.start();
+
+	};
+
+})();
---
+++ js/app/remind/webSQL.js
@@ -0,0 +1,139 @@
+﻿/// <reference path="Scripts/jquery-1.4.1-vsdoc.js" />
+
+var webStorage = {};
+webStorage.webSql = function () {
+
+    var _this = this;
+
+    //数据库
+    var _dataBase;
+
+    //打开数据库连接或者创建数据库
+    this.openDatabase = function () {
+
+        if (!!_dataBase) {
+            return _dataBase;
+        }
+        _dataBase = openDatabase("album", "1.0", "相册", 1024 * 1024, function () { });
+
+          if (!_dataBase) {
+              //alert("数据库创建失败！");
+          } else {
+              alert("数据库创建成功！");
+          }
+        return _dataBase;
+
+    }
+
+
+
+
+    //创建数据表
+    this.createTable = function () {
+
+        var dataBase = _this.openDatabase();
+        // 创建表
+        dataBase.transaction(function (tx) {
+            tx.executeSql(
+        "create table if not exists imagelist (id REAL UNIQUE, name TEXT)",
+        [],
+        function () {
+      	//alert('创建imagelist表成功');
+        },
+        function (tx, error) {
+            alert('创建imagelist表失败:' + error.message);
+        });
+        });
+    }
+
+    //添加数据
+    this.insert = function (name) {
+        var dataBase = _this.openDatabase();
+        var id = Math.random();
+        dataBase.transaction(function (tx) {
+            tx.executeSql(
+        "insert into imagelist (id, name) values(?, ?)",
+        [id, name],
+        function () {
+        //	alert('添加数据成功'); 
+        },
+        function (tx, error) {
+            alert('添加数据失败: ' + error.message);
+        });
+        });
+
+    }
+
+    // 查询
+    this.query = function () {
+        var dataBase = _this.openDatabase();
+        dataBase.transaction(function (tx) {
+            tx.executeSql(
+        "select * from imagelist", [],
+         function (tx, result) {
+             //result：SQLResultSet对象。 
+             //其定义为：interface SQLResultSet {
+             //  readonly attribute long insertId;
+             //  readonly attribute long rowsAffected;
+             //  readonly attribute SQLResultSetRowList rows;
+             //};
+                          alert(result);
+             for (var i = 0; i < result.rows.length; i++) {
+
+
+                 var id = result.rows.item(i)['id'];
+                 var name = result.rows.item(i)['name'];
+console.log(name);
+
+             }
+         },
+        function (tx, error) {
+            alert('查询失败: ' + error.message);
+        });
+        });
+
+    }
+
+    //更新数据
+    this.update = function (id, name) {
+
+        var dataBase = _this.openDatabase();
+        dataBase.transaction(function (tx) {
+            tx.executeSql(
+        "update imagelist set name = ? where id= ?",
+        [name, id],
+         function (tx, result) {
+             _this.query();
+         },
+        function (tx, error) {
+            alert('更新失败: ' + error.message);
+        });
+        });
+    }
+
+    //删除数据
+    this.del = function (id) {
+        var dataBase = _this.openDatabase();
+        dataBase.transaction(function (tx) {
+            tx.executeSql(
+        "delete from  imagelist where id= ?",
+        [id],
+         function (tx, result) {
+
+         },
+        function (tx, error) {
+            alert('删除失败: ' + error.message);
+        });
+        });
+    }
+
+    //删除数据表
+    this.dropTable = function () {
+        var dataBase = _this.openDatabase();
+        dataBase.transaction(function (tx) {
+            tx.executeSql('drop  table  imagelist');
+        });
+    }
+
+
+}
