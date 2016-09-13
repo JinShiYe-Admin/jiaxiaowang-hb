9fe83ec2151bd8757d786b04b0dce670342aa60e
songyanming <1062608083@qq.com>
2016-09-13 09:29:40 +0800

    班级界面加号添加班内发帖和上传图片功能 整理代码

--- js/app/tab_home.js
+++ js/app/tab_home.js
@@ -3,67 +3,93 @@ mui.init({
 });
 
 mui.plusReady(function() {
-mui(".mui-table-view").on('tap','.mui-table-view-cell',function(){
-  //获取id
-  var id = this.getAttribute("id");
-  console.log("1");
-  switch (id)
-{
-//case "up1":
-//	mui.openWindow({
-//  id:"newPost",
-//  url:'html/remind/newPost.html'
-//});
-//break;
-//case "up2":
-//	mui.openWindow({
-//  id:"newPost",
-//  url:'html/remind/upImage.html'
-//});
-//break;
-case "my_job":
-console.log("2");
-////把子页的路径写在数组里面
-//var subpages = ['tab_home.html', 'tab_class.html', 'tab_application.html', 'tab_me.html'];
-//	//显示目标选项卡
-//	plus.webview.show(subpages[1]);
-//	//隐藏当前选项卡
-//	plus.webview.hide(subpages[0]);
+	mui(".mui-table-view").on('tap', '.mui-table-view-cell', function() {
+			//获取id
+			var id = this.getAttribute("id");
+			switch(id) {
 
-  break;
-case "campus_notice":
-	mui.openWindow({
-		id:'touPiao',
-		url:'html/remind/campusNotice.html',
+				case "campus_notice":
+					mui.openWindow({
+						id: 'touPiao',
+						url: 'html/remind/campusNotice.html',
 						show: {
-								aniShow: 'pop-in'
-							}
+							aniShow: 'pop-in'
+						}
+					});
+					break;
+				case "class_post":
+					mui.openWindow({
+						id: "classPost",
+						url: 'html/remind/classPost.html'
+					});
+					break;
+				case "xiaoBo":
+					mui.openWindow({
+						id: "xiaoBo",
+						url: 'html/application/xiaoBo.html'
+					});
+					break;
+				case "sunshine_sports":
+					mui.openWindow({
+						id: "sunSports",
+						url: 'html/remind/sunSports.html'
+					});
+					break;
+
+			}
+
+		})
+		//点击我的作业
+	document.getElementById("my_job").addEventListener('tap', function() {
+		//获得主页面的webview
+		var main = plus.webview.currentWebview().parent();
+		//触发主页面的goemai事件
+		mui.fire(main, 'goemai');
+
+	});
+	/*点击加号触发*/
+	document.getElementById('openPopover').addEventListener('tap', function() {
+		if(mui.os.plus) {
+			var a = [{
+				title: "班内发帖"
+			}, {
+				title: "上传照片"
+			}];
+			plus.nativeUI.actionSheet({
+				title: "选择发布内容",
+				cancel: "取消",
+				buttons: a
+			}, function(b) { /*actionSheet 按钮点击事件*/
+				switch(b.index) {
+					case 0:
+						break;
+					case 1:
+						newPost(); /*班内发帖*/
+						break;
+					case 2:
+						upImage(); /*上传照片*/
+						break;
+					default:
+						break;
+				}
+			})
+		}
+	}, false);
+	//拍照 
+	function newPost() {
+		mui.openWindow({
+			id: "newPost",
+			url: 'html/remind/newPost.html'
 		});
-  break;
-case "class_post":
-	mui.openWindow({
-    id:"classPost",
-    url:'html/remind/classPost.html'
-  });
-  break;
-case "xiaoBo":
-	mui.openWindow({
-    id:"xiaoBo",
-    url:'html/application/xiaoBo.html'
-  });
-  break;
-case "sunshine_sports":
-	mui.openWindow({
-    id:"sunSports",
-    url:'html/remind/sunSports.html'
-  });
-  break;
 
-}
-  
+	}
 
-
-})
+	function upImage() {
+		mui.openWindow({
+			id: "upImage",
+			url: 'html/remind/upImage.html'
+		});
+	}
 
 	//点击左上角图标，打开帮助；
 	document.getElementById('help').addEventListener('tap', function() {
@@ -75,4 +101,3 @@ case "sunshine_sports":
 		});
 	});
 });
-
